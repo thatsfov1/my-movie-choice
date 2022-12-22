37664d8049745925e55638b9e5edad3bd499ebb3
thatsfov1 <zheniaukr@icloud.com>
2022-12-22 07:44:54 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SimpleBottomNavigation from "./components/Navigation/Navigation.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+
+
+
+    </div>
+  )
+}
+
+export default App
